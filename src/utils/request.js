16f80b4d4298f9a490fd5a7e47737917fd4af66b
wubai <imwubau@gmail.com>
2020-09-16 16:27:23 +0800

    add

--- src/utils/request.js
+++ src/utils/request.js
@@ -25,7 +25,7 @@ axios.interceptors.response.use(function(response) {
   }
   // Any status codes that falls outside the range of 2xx cause this function to trigger
   // Do something with response error
-  // return Promise.reject(error)
+  return Promise.reject(error)
 })
 
 export {

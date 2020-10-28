b10ca86ef6d25fa0765e0847feeff1df9cb84b18
wubai <imwubau@gmail.com>
2020-10-28 16:15:22 +0800

    add

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 
 // 环境变量
-const apiDomain = '' // 正式环境用跟目录去读取接口
+const apiDomain = 'http://speakfan.net:12345' // 正式环境用跟目录去读取接口
 // const apiDomain = 'http://192.168.1.65:8080'
 
 export {
--- src/views/list/index.vue
+++ src/views/list/index.vue
@@ -37,7 +37,6 @@
       <el-table-column prop="smallvideo" label="小场景" width="150">
         <template slot-scope="scope">
           <el-button
-            v-if="scope.row.small_objects && scope.row.small_objects.length"
             type="text"
             size="medium"
             @click="viewImage(scope.row, 'small')"
@@ -93,46 +92,6 @@ export default {
     this.fetchData()
     clearInterval(intervalTimer)
     this.autoRefreshChange(true)
-    // 下面是模拟数据
-    // this.listLoading = false
-    // this.tableData = [
-    //   {
-    //     'filename': 'big_20200904121759_00000077.jpg', // # 大场景或者中场景截图名，
-    //     // # big或者middle表示场景  20200904121759时间字符串年2020-09-04 12：17：59 00000077第77个图
-    //     'location': 'abc', // # 摄像机安装的地区位置
-    //     'time': '20200904121759', // #时间，同文件名里
-    //     'isred': 1, // #此图是否是红灯
-    //     'redline': 10, // #红灯线在图的比例，从下算
-    //     'overline': 5, // #越线线在图的比例，从下算
-    //     'cameradirection': 0, // #摄像头朝向，0对脸，1对背
-    //     'smallvideo': 'big_20200904121759_00000077-small.jpg', // #小场景截图名，不一定存在要根据small_objects里是否有内容判读
-    //     'objects': [ // #大中场景里判别的违法对象
-    //       { 'illegal': 'redline', 'name': 'motorbike', 'left': 1547, 'top': 631, 'right': 1720, 'bottom': 847 },
-    //       // # 违法方式，            违法对象：摩托，自行车  在图中的坐标，左，上，右，下
-    //       { 'illegal': 'redline', 'name': 'motorbike', 'left': 2006, 'top': 745, 'right': 2156, 'bottom': 940 }
-    //     ],
-    //     'small_objects': [
-    //       { 'illegal': 'redline', 'name': 'motorbike', 'left': 1685, 'top': 588, 'right': 2060, 'bottom': 1355 }
-    //     ]
-    //   },
-    //   {
-    //     'filename': 'big_20200904121759_00000077.jpg', // # 大场景或者中场景截图名，
-    //     // # big或者middle表示场景  20200904121759时间字符串年2020-09-04 12：17：59 00000077第77个图
-    //     'location': 'abc', // # 摄像机安装的地区位置
-    //     'time': '20200904121759', // #时间，同文件名里
-    //     'isred': 1, // #此图是否是红灯
-    //     'redline': 10, // #红灯线在图的比例，从下算
-    //     'overline': 5, // #越线线在图的比例，从下算
-    //     'cameradirection': 0, // #摄像头朝向，0对脸，1对背
-    //     'smallvideo': 'big_20200904121759_00000077-small.jpg', // #小场景截图名，不一定存在要根据small_objects里是否有内容判读
-    //     'objects': [ // #大中场景里判别的违法对象
-    //       { 'illegal': 'redline', 'name': 'motorbike', 'left': 1547, 'top': 631, 'right': 1720, 'bottom': 847 }
-    //     ],
-    //     'small_objects': [
-    //       { 'illegal': 'redline', 'name': 'motorbike', 'left': 1685, 'top': 588, 'right': 2060, 'bottom': 1355 }
-    //     ]
-    //   }
-    // ]
   },
   destroyed() {
     clearInterval(intervalTimer)
@@ -165,37 +124,55 @@ export default {
     viewImage(row, type) {
       if (type === 'big') {
         this.isLookBigImg = true
-        const currentObjects = row.objects || []
+        // const currentObjects = row.objects || []
+        // setTimeout(() => {
+        //   const img = document.createElement('img')
+        //   img.src = `${apiDomain}/static/${row.filename}`
+        //   img.style.width = '1000px'
+        //   img.onload = e => {
+        //     currentObjects.forEach(item => {
+        //       const { naturalWidth, clientWidth } = e.path[0]
+        //       const proportion = (naturalWidth / clientWidth).toFixed(2)
+        //       const boxWidth = item.right - item.left
+        //       const boxheight = item.bottom - item.top
+        //       const div = document.createElement('div')
+        //       div.setAttribute('class', 'bigImgredBox')
+        //       div.style.position = 'absolute'
+        //       div.style.left = (item.left / proportion).toFixed(2) + 'px'
+        //       div.style.top = (item.top / proportion).toFixed(2) + 'px'
+        //       div.style.width = (boxWidth / proportion).toFixed(2) + 'px'
+        //       div.style.height = (boxheight / proportion).toFixed(2) + 'px'
+        //       div.style.border = '2px solid #f00'
+        //       document.querySelector('#dialog_img_box').appendChild(div)
+        //     })
+        //   }
+        //   document.querySelector('#dialog_img_box').appendChild(img)
+        // }, 100)
         setTimeout(() => {
-          const img = document.createElement('img')
-          img.src = `${apiDomain}/static/${row.filename}`
-          img.style.width = '1000px'
-          img.onload = e => {
-            currentObjects.forEach(item => {
-              const { naturalWidth, clientWidth } = e.path[0]
-              const proportion = (naturalWidth / clientWidth).toFixed(2)
-              const boxWidth = item.right - item.left
-              const boxheight = item.bottom - item.top
-              const div = document.createElement('div')
-              div.setAttribute('class', 'bigImgredBox')
-              div.style.position = 'absolute'
-              div.style.left = (item.left / proportion).toFixed(2) + 'px'
-              div.style.top = (item.top / proportion).toFixed(2) + 'px'
-              div.style.width = (boxWidth / proportion).toFixed(2) + 'px'
-              div.style.height = (boxheight / proportion).toFixed(2) + 'px'
-              div.style.border = '2px solid #f00'
-              document.querySelector('#dialog_img_box').appendChild(div)
-            })
-          }
-          document.querySelector('#dialog_img_box').appendChild(img)
+          (row.objects || []).forEach((item) => {
+            if (item.file) {
+              const img = document.createElement('img')
+              img.src = `${apiDomain}/static/tmpfs/${item.file}`
+              img.style.width = '1000px'
+              document.querySelector('#dialog_img_box').appendChild(img)
+            }
+          })
         }, 100)
       } else {
         this.isLookBigImg = false
         setTimeout(() => {
-          const img = document.createElement('img')
-          img.src = `${apiDomain}/static/${row.smallvideo}`
-          img.style.width = '1000px'
-          document.querySelector('#dialog_img_box').appendChild(img)
+          (row.objects || []).forEach((item) => {
+            if (item.small_file) {
+              const img = document.createElement('img')
+              img.src = `${apiDomain}/static/tmpfs/${item.small_file}`
+              img.style.width = '1000px'
+              document.querySelector('#dialog_img_box').appendChild(img)
+            }
+          })
+          // const img = document.createElement('img')
+          // img.src = `${apiDomain}/static/${row.smallvideo}`
+          // img.style.width = '1000px'
+          // document.querySelector('#dialog_img_box').appendChild(img)
         }, 100)
       }
       this.dialogVisible = true
